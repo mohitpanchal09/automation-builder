ae2f1d236f210011536fa0f364dd11895faf5d2d
mohitpanchal09 <panchalmohitg2002@gmail.com>
2024-10-10 16:49:40 +0530

    changes

--- src/app/(main)/(pages)/workflows/page.tsx
+++ src/app/(main)/(pages)/workflows/page.tsx
@@ -1,13 +1,16 @@
 import React from 'react'
+import WorkflowButton from './_components/workflow-button'
 
 type Props = {}
 
 export default function Workflows({ }: Props) {
   return (
-    <div className='flex flex-col gap-4 '>  
-     <h1 className="text-4xl sticky top-0 z-[10] p-6  bg-background/50 backdrop-blur-lg flex items-center border-b">
-      <span>Workflows</span>
-    </h1>
+    <div className="flex flex-col relative">
+      <h1 className="text-4xl sticky top-0 z-[10] p-6 bg-background/50 backdrop-blur-lg flex items-center border-b justify-between">
+        Workflows
+        <WorkflowButton />
+      </h1>
+
     </div>
   )
 }
