422521dbd136e19119e3effd77c19a4309e8ea33
mohitpanchal09 <panchalmohitg2002@gmail.com>
2024-10-10 14:59:28 +0530

    changes

--- src/app/(main)/(pages)/workflows/page.tsx
+++ src/app/(main)/(pages)/workflows/page.tsx
@@ -2,10 +2,10 @@ import React from 'react'
 
 type Props = {}
 
-export default function Workflows({}: Props) {
+export default function Workflows({ }: Props) {
   return (
-    <div className='flex flex-col gap-4'>
-    <h1 className="text-4xl sticky top-0 z-[10] p-6  bg-background/50 backdrop-blur-lg flex items-center border-b">
+    <div className='flex flex-col gap-4 '>  
+     <h1 className="text-4xl sticky top-0 z-[10] p-6  bg-background/50 backdrop-blur-lg flex items-center border-b">
       <span>Workflows</span>
     </h1>
     </div>
