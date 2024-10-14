71cca8ef7148bfdc55fe6611630ccaa3f05b4018
mohitpanchal09 <panchalmohitg2002@gmail.com>
2024-10-14 23:01:43 +0530

    changes

---
+++ src/providers/editor-provider.tsx
@@ -0,0 +1,13 @@
+"use client"
+
+import React from 'react'
+
+type Props = {}
+
+const EditorProvider = (props: Props) => {
+  return (
+    <div>EditorProvider</div>
+  )
+}
+
+export default EditorProvider
