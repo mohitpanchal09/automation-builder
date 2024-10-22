3859c3705c9062cdb3acc49ee6561866e9acb3d4
mohitpanchal09 <panchalmohitg2002@gmail.com>
2024-10-22 08:38:08 +0530

    changes

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,10 +1,11 @@
 import type { Metadata } from "next";
-import {DM_Sans} from 'next/font/google'
+import { DM_Sans } from 'next/font/google'
 import "./globals.css";
 import { ThemeProvider } from "@/providers/theme-provider";
 import { ClerkProvider } from "@clerk/nextjs"
 import ModalProvider from "@/providers/modal-provider";
-const font = DM_Sans({subsets:['latin']})
+import { Toaster } from "@/components/ui/sonner";
+const font = DM_Sans({ subsets: ['latin'] })
 
 export const metadata: Metadata = {
   title: "Fuzzie",
@@ -18,21 +19,23 @@ export default function RootLayout({
 }>) {
   return (
     <ClerkProvider publishableKey={process.env.NEXT_PUBLIC_CLERK_PUBLISHABLE_KEY}>
-    <html lang="en">
-      <body
-        className={font.className}
-      >
-        <ThemeProvider
+      <html lang="en">
+        <body
+          className={font.className}
+        >
+          <ThemeProvider
             attribute="class"
             defaultTheme="dark"
             enableSystem
             disableTransitionOnChange
           >
-         <ModalProvider>  {children}</ModalProvider> 
+            <ModalProvider>  {children}
+              <Toaster />
+            </ModalProvider>
           </ThemeProvider>
-        
-      </body>
-    </html>
+
+        </body>
+      </html>
     </ClerkProvider>
   );
 }
